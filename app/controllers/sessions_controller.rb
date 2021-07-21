d445a1160840a01280035e5245efb28a40067314
code salley <juxsalley@gmail.com>
2021-07-21 15:18:37 +0000

    change schema to use bcrypt for password field

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,19 @@ class SessionsController < ApplicationController
         @user = User.new
     end
     def new_session 
+        user = User.create(user_params)
+        p user 
+        if user.save
+            p 'saved'
+            session[user_id] = user.id
+            redirect_to registration_path
+        else
+          render  :signup
+        end
+    end
+
+    private 
+    def user_params 
+        params.require(:user).permit(:first_name, :last_name, :middle_name, :address, :phone_number, :password, :password_confirmation, :donor)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_131847) do
+ActiveRecord::Schema.define(version: 2021_07_21_151616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(version: 2021_07_21_131847) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
-    t.string "password_confirmation"
     t.string "phone_number"
     t.string "address"
     t.string "first_name"
@@ -36,6 +34,8 @@ ActiveRecord::Schema.define(version: 2021_07_21_131847) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "verified", default: false
+    t.string "tmp_code"
+    t.string "password_digest"
   end
 
 end
