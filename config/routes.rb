21d3c74c2dd9afaedb91af457375e304a5378d76
code salley <juxsalley@gmail.com>
2021-07-21 13:42:05 +0000

    use floating forms from bootsrap instead of normal form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/registration', to: "sessions#signup"
+  post '/registration', to: "sessions#new_session"
   get '/profile', to: 'home#user_profile'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_130219) do
+ActiveRecord::Schema.define(version: 2021_07_21_131847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_21_130219) do
     t.boolean "activated", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "verified", default: false
   end
 
 end
